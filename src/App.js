fdcb395768dc353add0b6f28cd59471521bb3fed
Rutkar bhat <80646853+rutkar11@users.noreply.github.com>
2023-11-09 21:32:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,9 @@ import Checkout from './Checkout';
 import Login from './login';
 import { auth } from './firebase';
 import { useStateValue } from './StateProvider';
-import Payment from './Payment';
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
 // import Orders from './Orders';
 
-const promise=loadStripe(
-  "pk_test_51O4hm0SBWHWsGJ4t02V5OxRDG1YKnjdTGVSSpDjP9aeVQmEt4vVAvoVvGu6mzsmLKKmZ6TVITu7Fn7Y6UmH2gqqn00dFgS9YF1"
-  );
+
 
 function App() {
  const [{basket, user},dispatch]=useStateValue();
@@ -58,10 +53,6 @@ function App() {
             <Header />
             <Checkout />
            </Route>
-           <Route path="/payment">
-            <Header />
-            <Elements stripe={promise}>
-            <Payment />
             </Elements>
             
            </Route>
