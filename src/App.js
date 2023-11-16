2110f9440cffa0bad0c955767cedc58b820b85ef
Rutkar bhat <80646853+rutkar11@users.noreply.github.com>
2023-11-16 17:34:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,63 +1,55 @@
-import React,{useEffect} from 'react';
-import './App.css';
-import Header from './header';
-import Home from './Home';
+import React, { useEffect } from "react";
+import "./App.css";
+import Header from "./header";
+import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Checkout from './Checkout';
-import Login from './login';
-import { auth } from './firebase';
-import { useStateValue } from './StateProvider';
-// import Orders from './Orders';
-
-
+import Checkout from "./Checkout";
+import Login from "./login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
- const [{basket, user},dispatch]=useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
 
-  useEffect(()=>{
+  useEffect(() => {
     // will only run once when app comp. loads
 
-   auth.onAuthStateChanged(authUser=>{
-    // console.log('THE USER IS >>>',authUser);
-    if(authUser){
-      // the user just logged in
-      dispatch({
-        type: 'SET_USER',
-        user: authUser
-      })
-
-    }
-    else{
-       // the user was logged out
-       dispatch({
-        type: 'SET_USER',
-        user: null
-       })
-    }
-
-   })
-  },[])
+    auth.onAuthStateChanged((authUser) => {
+      // console.log('THE USER IS >>>',authUser);
+      if (authUser) {
+        // the user just logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user was logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
 
   return (
     <Router>
       <div className="App">
-       <Switch>
+        <Switch>
           <Route path="/orders">
-           <Header />
-           {/* <Orders />  */}
+            <Header />
+            {/* <Orders /> */}
           </Route>
           <Route path="/login">
-             <Login />
+            <Login />
           </Route>
           <Route path="/checkout">
             <Header />
             <Checkout />
-           </Route>
-            </Elements>
-            
-           </Route>
-           <Route path="/">
-            <Header /> 
+          </Route>
+          <Elements /> {/* Added closing tag for Elements */}
+          <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
